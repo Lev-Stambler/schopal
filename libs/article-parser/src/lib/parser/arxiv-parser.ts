952542cb4e33581ace6300a5904b4bb3c2468088
Lev Stambler <levst@google.com>
2020-07-10 09:40:17 -0400

    change to pdf explorer

--- libs/article-parser/src/lib/parser/arxiv-parser.ts
+++ libs/article-parser/src/lib/parser/arxiv-parser.ts
@@ -4,8 +4,7 @@ import {
   ParsedArticleParagraph,
   ArxivOptions,
 } from '@foodmedicine/interfaces';
-import fetch from 'node-fetch'
-import pdf from 'pdf-parse'
+import { getParagraphsFromPDFUrl } from '@foodmedicine/pdf-explorer';
 
 /**
  * A parser for https://www.ebi.ac.uk/europepmc/webservices/rest/
@@ -15,12 +14,8 @@ export const ArxivParser: Parser<ParsedArticle> = {
     if (!opts?.parsedArticleHead) {
       throw 'Please add in the parsed head';
     }
+    const paragraphTexts = await getParagraphsFromPDFUrl(fileUrl);
     try {
-      const fetchRes = await fetch(fileUrl);
-      const buff = await fetchRes.buffer()
-      const pdfData = await pdf(buff)
-      console.log(pdfData);
-      const paragraphTexts: string[] = pdfData.text.split('.');
       const paragraphs: ParsedArticleParagraph[] = paragraphTexts.map(
         (paragraphText) =>
           opts.getCorrelationScore(
