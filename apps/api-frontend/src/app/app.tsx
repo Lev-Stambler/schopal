15b897f2d207b9dea4f5a90d6c3e516220885313
Lev Stambler <levst@google.com>
2020-07-20 16:23:03 -0400

    footer

--- apps/api-frontend/src/app/app.tsx
+++ apps/api-frontend/src/app/app.tsx
@@ -4,7 +4,7 @@ import './app.css';
 
 import SearchPage from './pages/search-pages/search';
 import ResultsPage from './pages/search-pages/search-result';
-import InfoPage from './pages/info-page/info'
+import InfoPage from './pages/info-page/info';
 
 export const App = () => {
   /*
@@ -23,6 +23,12 @@ export const App = () => {
           </Switch>
         </Router>
       </main>
+      <div className="footer">
+        <div className="feedback-form">
+          <a href="https://forms.gle/gCkYrsvmBRGRbJKc9">Leave feedback</a>
+        </div>
+        <div className="contact">Please contact me by email: levst@google.com</div>
+      </div>
     </div>
   );
 };
